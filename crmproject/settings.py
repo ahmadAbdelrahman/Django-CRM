aba0e1021563481fe8764236d84fe676a3b9c47e
ahmed <ahmedhamed1986@gmail.com>
2025-04-26 13:43:04 +0300

    first commit

--- crmproject/settings.py
+++ crmproject/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,11 +80,11 @@ WSGI_APPLICATION = 'crmproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'crmdatabase',
-        'USER' : 'root',
-        'PASSWORD' : 'helloAhmed76152',
-        'HOST' : 'localhost',
-        'PORT' : '3306',
+        'NAME': os.getenv('NAME'),
+        'USER' : os.getenv('USER'),
+        'PASSWORD' : os.getenv('PASSWORD'),
+        'HOST' : os.getenv('HOST'),
+        'PORT' : os.getenv('PORT'),
     }
 }
 
--- mydb.py
+++ mydb.py
@@ -1,10 +1,24 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+
+# database = mysql.connector.connect(
+#     host = 'localhost',
+#     user = 'root',
+#     passwd = 'helloAhmed76152'
+
+# )
+
+print ('HOSt IS: ', os.environ.get('HOST'))
+print ('HOSt2 IS: ', os.getenv('HOST'))
 
 database = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    passwd = 'helloAhmed76152'
+    host = os.getenv('HOST'),
+    user =  os.getenv('USER'),
+    passwd = os.getenv('PASSWORD'),
 
 )
 
