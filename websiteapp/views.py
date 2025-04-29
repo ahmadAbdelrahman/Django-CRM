3b5954bd48f29445fd0b2af1190f6c341240ed53
ahmed <ahmedhamed1986@gmail.com>
2025-04-29 21:41:13 +0300

    registration

--- websiteapp/views.py
+++ websiteapp/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
-
 # Create your views here.
 def index(request):
-    return render (request, 'index.html', {})
+
+    # check to see if the user is logged in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have logged in!')
+            return redirect('home')
+        else:
+            messages.success(request, 'Error')
+            return redirect('home')
+    else:
+        return render(request, 'index.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have logged out!')
+    return redirect('home')
+
+
+def regestire_user(request):
+    return render(request, 'register.html', {})
